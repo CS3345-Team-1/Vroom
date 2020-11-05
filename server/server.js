f85c6ab917eedc64b1a4aaaa22d9cc3adf03a8c5
Randolph Rankin <rhrankin@smu.edu>
2020-11-04 21:51:08 -0600

    backend/frontend testing

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');const app = express();const port = process.env.PORT || 5000;
+//Route setup
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+//Start server
+app.listen(port, (req, res) => {
+    console.log(`server listening on port: ${port}`)
+});
